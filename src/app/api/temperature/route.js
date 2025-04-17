1166340704e20672b903fa59fd848e47c9ed58b3
Leo <alvarezleandro720@gmail.com>
2025-04-17 00:25:32 -0400

    refresh updates data

--- src/app/api/temperature/route.js
+++ src/app/api/temperature/route.js
@@ -1,8 +1,16 @@
 import { getButterflyDataTemperature} from "@/app/lib/butterfly"
 import { NextResponse } from "next/server"
+import { refreshSettings } from "@/app/lib/butterfly";
 
 // pull butterfly data from DB
 export async function GET(req){
+    const { searchParams } = new URL(req.url);
+    const refresh = searchParams.get('refresh') === 'true';
+    
+    if (refresh) {
+        // Force refresh the data
+        refreshSettings.refresh=true
+    }
     try{
         const butterfly_data = await getButterflyDataTemperature()
         return NextResponse.json({message:"success",data:butterfly_data},{status:200})
--- src/app/lib/butterfly.js
+++ src/app/lib/butterfly.js
@@ -2,31 +2,38 @@ import { connect_db } from "@/app/lib/db_setup"
 
 const butterfly_collection = await connect_db()
 
-//use cursor object to make this faster?
 let butterfly_data;
+//turn into object to import and modify inside api files
+export let refreshSettings = {
+    refresh: false
+}
 const getButterflyData = async()=>{
     const butterfly_data_cursor = butterfly_collection.find()
     const butterfly_data_result = await butterfly_data_cursor.toArray()
     butterfly_data = butterfly_data_result
 }
 export const getButterflyDataTemperature = async()=>{
-    if(!butterfly_data) await getButterflyData()
+    if(!butterfly_data || refreshSettings.refresh) await getButterflyData()
+    refreshSettings.refresh = false
     return butterfly_data.map((butterfly)=>({"date":butterfly.date,"temperature":butterfly.temperature,"species":butterfly.species}))
 
 }
 export const getButterflyDataHumidity = async()=>{
-    if(!butterfly_data) await getButterflyData()
+    if(!butterfly_data || refreshSettings.refresh) await getButterflyData()
+    refreshSettings.refresh = false
     return butterfly_data.map((butterfly)=>({"date":butterfly.date,"humidity":butterfly.humidity,"species":butterfly.species}))
 
 }
 export const getButterflyDataWindSpeed = async()=>{
-    if(!butterfly_data) await getButterflyData()
+    if(!butterfly_data|| refreshSettings.refresh) await getButterflyData()
+    refreshSettings.refresh = false
     return butterfly_data.map((butterfly)=>({"date":butterfly.date,"wind_speed":butterfly.wind_speed,"species":butterfly.species}))
 
 }
 
 export const getSpeciesMakeupData = async()=>{
-    if(!butterfly_data) await getButterflyData()
+    if(!butterfly_data || refreshSettings.refresh) await getButterflyData()
+    refreshSettings.refresh = false
     //aggregate : group by the id -> species. Then get their counts by each id. Lastly project by switching names to value and label
     const pie_chart_data_cursor = butterfly_collection.aggregate(
                 [
